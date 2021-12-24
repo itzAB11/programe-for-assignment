f11ba5700f90f66ab0e7949b333489eda324243f
itzAB11 <96369687+itzAB11@users.noreply.github.com>
2021-12-24 20:05:54 +0500

    Add files via upload

---
+++ iftikhar sir Assignment.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	
+cout<<"********HELLO WORLD*******"<<endl;
+int a,b;
+cout<<"Enter value for first number  : ";
+cin>>a;
+cout<<endl;
+cout<<"Enter value for second number : ";
+cin>>b;
+cout<<endl;
+cout<<"Addition of these Number : "<<a+b;
+
+return 0;
+}
+
